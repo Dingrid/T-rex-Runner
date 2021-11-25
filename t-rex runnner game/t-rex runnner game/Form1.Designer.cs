9db3ecfc794480fe2ca1b110a2222a8d328014f0
Ingrid <50678427+Dingrid@users.noreply.github.com>
2021-11-25 16:13:18 -0300

    adicionadno t-rex

---
+++ t-rex runnner game/t-rex runnner game/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace t_rex_runnner_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trex = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtBestScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trex
+            // 
+            this.trex.Image = global::t_rex_runnner_game.Properties.Resources.running;
+            this.trex.Location = new System.Drawing.Point(48, 207);
+            this.trex.Name = "trex";
+            this.trex.Size = new System.Drawing.Size(40, 43);
+            this.trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.trex.TabIndex = 1;
+            this.trex.TabStop = false;
+            this.trex.Tag = "trex";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::t_rex_runnner_game.Properties.Resources.obstacle_1;
+            this.pictureBox2.Location = new System.Drawing.Point(236, 204);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "obstaculo";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::t_rex_runnner_game.Properties.Resources.obstacle_2;
+            this.pictureBox3.Location = new System.Drawing.Point(280, 217);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstaculo";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtScore.Location = new System.Drawing.Point(13, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(75, 20);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Pontos: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // txtBestScore
+            // 
+            this.txtBestScore.AutoSize = true;
+            this.txtBestScore.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtBestScore.Location = new System.Drawing.Point(435, 15);
+            this.txtBestScore.Name = "txtBestScore";
+            this.txtBestScore.Size = new System.Drawing.Size(136, 17);
+            this.txtBestScore.TabIndex = 5;
+            this.txtBestScore.Text = "Melhor pontuação: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(-5, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(774, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "____________________________________________________________________________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(603, 297);
+            this.Controls.Add(this.txtBestScore);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.trex);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T-Rex Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox trex;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label txtBestScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ t-rex runnner game/t-rex runnner game/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace t_rex_runnner_game
+{
+    public partial class Form1 : Form
+    {
+        //variaveis globais
+        bool jumping = false; //se esta pulando ou n
+        int jumpSpeed = 12; 
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rand = new Random();
+        int position; //posicao do obstaculo
+        bool isGameOver = false;
+        int bestScore = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameReset(); //inicia o jogo
+        }
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+            trex.Top += jumpSpeed; //trex vai pra cima
+
+            txtScore.Text = "Pontos: " + score;
+
+            //configuracoes do pulo
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            //retorna com as configurações padrões
+            if (trex.Top > 206 && jumping == false)
+            {
+                force = 12;
+                trex.Top = 207;
+                jumpSpeed = 0;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstaculo")
+                {
+                    x.Left -= obstacleSpeed; //movimento do obstaculo
+
+                    if (x.Left < -100) //quando os cactos estiverem <<<<
+                    {
+                        //gera um cacto novo e adiciona o score
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++;
+                    }
+
+                    if (trex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        trex.Image = Properties.Resources.dead;
+                        SoundPlayer hit = new SoundPlayer(@"C:\Users\isaso\Documents\Ingrid\FATEC SP\4 sem\ILP580\t rex\imagens\Resources_hit.wav");
+                        hit.Play();
+                        txtScore.Text += " Pressione R para reiniciar o jogo!";
+                        isGameOver = true;
+                        if(bestScore < score)
+                        {
+                            bestScore = score;
+                            txtBestScore.Text = "Melhor pontuação: " + bestScore;
+                        }
+
+                    }
+                }
+            }
+
+            //niveis de dificuldade
+            if (score > 5 && score<10) //+- facil
+            {
+                obstacleSpeed = 13;
+            }
+            if (score > 10 && score<20) //medio
+            {
+                obstacleSpeed = 15;
+            }
+            if (score > 20) //dificil
+            {
+                obstacleSpeed = 18;
+            }
+
+
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e) //tecla pressionada
+        {
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+                SoundPlayer jumpS = new SoundPlayer(@"C:\Users\isaso\Documents\Ingrid\FATEC SP\4 sem\ILP580\t rex\imagens\Resources_button-press.wav");
+                jumpS.Play();
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e) //tecla nao pressionada
+        {
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+
+            if (e.KeyCode == Keys.R && isGameOver == true) //caso perca a partida e queira reiniciar
+            {
+                gameReset();
+            }
+        }
+
+        private void gameReset()
+        {
+            jumping = false;
+            force = 12;
+            jumpSpeed = 0;
+            score = 0;
+            obstacleSpeed = 10;
+            txtScore.Text = "Pontos: " + score;
+            txtBestScore.Text = "Melhor pontuação: " + bestScore;
+            trex.Image = Properties.Resources.running; //gif do t rex andando
+            trex.Top = 207;//y location
+            isGameOver = false;
+            
+
+            foreach (Control x in this.Controls)
+            {
+                //posicao aleatoria dos obstaculos
+                if (x is PictureBox && (string)x.Tag == "obstaculo") 
+                {
+                    position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+
+                    x.Left = position;
+                }
+            }
+
+            gameTimer.Start();
+        }
+    }
+}
